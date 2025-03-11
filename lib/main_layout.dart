46800e69f5db16a75ebc961634eea79ec9be81de
Motaz <motazyusuf@gmail.com>
2025-03-11 16:05:10 +0200

    -Adding new screen to navigation bar

--- lib/main_layout.dart
+++ lib/main_layout.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task_manager/core/configs/theme/app_colors.dart';
 import 'package:task_manager/core/constants/strings.dart';
+import 'package:task_manager/features/news/presentation/pages/news_screen.dart';
 import 'package:task_manager/features/tasks/data/repositories/task_repository.dart';
 import 'package:task_manager/features/tasks/presentation/manager/task_bloc.dart';
 import 'package:task_manager/features/tasks/presentation/pages/tasks_screen.dart';
@@ -15,7 +16,7 @@ class MainLayout extends StatefulWidget {
 
 class _MainLayoutState extends State<MainLayout> {
   int _currentIndex = 0;
-  final List<Widget> _screens = [TasksScreen(), Placeholder()];
+  final List<Widget> _screens = [TasksScreen(), NewsScreen()];
 
   @override
   Widget build(BuildContext context) {
